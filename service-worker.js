4dde1bfbd54f065e2d22fc2208d9dd5c73b8bd00
torresliyixin-png <torresliyixin@gmail.com>
2026-01-14 19:35:54 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'snake-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
